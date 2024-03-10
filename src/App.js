89f6cbd03ff9e2c616d3f43c8e29b7ed37ee92bb
duc minh <21ducminh@gmail.com>
2024-03-10 16:49:43 +0700

    UI: skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Brands from './components/Brands';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Brands />
       <About />
+      <Skills />
       <div style={{ height: '2000px' }}></div>
     </div>
   );
