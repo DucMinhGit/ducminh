32b45192129dfa95604d883bee92641ede426127
duc minh <21ducminh@gmail.com>
2024-03-10 22:18:32 +0700

    UI: footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About';
 import Brands from './components/Brands';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Portfolio from './components/Portfolio';
@@ -21,7 +22,7 @@ function App() {
       <Services />
       <Testimonials />
       <Contact />
-      <div style={{ height: '2000px' }}></div>
+      <Footer />
     </div>
   );
 }
