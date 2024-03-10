03cb8b3ac5709c0550f77cd4445786976675b981
duc minh <21ducminh@gmail.com>
2024-03-10 17:47:42 +0700

    UI: portfolio and projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Brands from './components/Brands';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Portfolio from './components/Portfolio';
 import Skills from './components/Skills';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Brands />
       <About />
       <Skills />
+      <Portfolio />
       <div style={{ height: '2000px' }}></div>
     </div>
   );
